1cf7befcd1f74c0c70a1bd77799e295619395928
zeuz <zeuz@zeuzs-iMac.local>
2023-07-18 15:25:02 -0400

    set up page change functionality using useContext and useState

--- src/App.js
+++ src/App.js
@@ -5,14 +5,35 @@ import Porfolio from './pages/Portfolio';
 import Resume from './pages/Resume';
 import Contact from './pages/Contact';
 import Footer from './components/Footer';
-import {useState} from "react"
+import {useState, createContext} from "react"
+
+export const AppContext = createContext()
 
 function App() {  
+  const [page, setPage] = useState("About")
+
+  const renderPage = () => {
+    if(page === "About") {
+      return <About />
+    }
+    if(page === "Portfolio") {
+      return <Porfolio />
+    }
+    if(page === "Resume") {
+      return <Resume />
+    }
+    if(page === "Contact") {
+      return <Contact />
+    }
+  }
+
   return (
     <div className="App">
+      <AppContext.Provider value={{setPage}}>
       <Header />
-
+        {renderPage()}
       <Footer />
+      </AppContext.Provider>
     </div>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { AppContext } from "../../App"
+
 
 function Header() {
+    const {setPage} = useContext(AppContext)
+
+    const handleClick = (event) => {
+        const pageValue = event.target.innerHTML
+        setPage(`${pageValue}`)
+    }
+
   return (
-    <div>Header</div>
+    <header>
+    <h1>Jesse Elkins</h1>
+    <nav>
+        <ul>
+            <li onClick={handleClick}>About</li>
+            <li onClick={handleClick}>Portfolio</li>
+            <li onClick={handleClick}>Resume</li>
+            <li onClick={handleClick}>Contact</li>
+        </ul>
+    </nav>
+    </header>
   )
 }
 
