611f321f57b63264f0f435c79ae939c34e506e51
zeuz <zeuz@zeuzs-iMac.local>
2023-07-18 15:09:48 -0400

    set up file structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Header from './components/Header';
+import About from './pages/About';
+import Porfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+import Contact from './pages/Contact';
+import Footer from './components/Footer';
+import {useState} from "react"
 
-function App() {
+function App() {  
   return (
     <div className="App">
- 
+      <Header />
+
+      <Footer />
     </div>
   );
 }
---
+++ src/pages/Portfolio/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Porfolio() {
+  return (
+    <div>Porfolio</div>
+  )
+}
+
+export default Porfolio
